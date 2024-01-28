648f23d97918199cefc1b17eb9f638da75442327
1000copy <1000copy@gmail.com>
2024-01-28 10:29:39 +0800

    project crud

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 //app.js
 App({
   onLaunch: function () {
+    wx.getStorage({
+      key: 'projects',
+      success (res) {
+        getApp().globalData.projects = res.data || getApp().globalData.initprojects
+      }
+    })
+  },
+  addproject:function(p){
+    if(!this.globalData.projects)
+      this.globalData.projects = this.globalData.initprojects
+    this.globalData.projects.push(p);
+    wx.setStorage({
+      key: 'projects',
+      data: getApp().globalData.projects,
+      })
+  },
+  reset:function(p){
+    this.globalData.projects = this.globalData.initprojects
+    wx.setStorage({
+      key: 'projects',
+      data: getApp().globalData.projects,
+      })
   },
   globalData: {
     preset:"p1",
@@ -8,11 +30,19 @@ App({
     currentProject:{},
     currentWork:{},
     currentItem:{},
-    projects:[
-      {n:"project1",r:"r1",works:[
-        {n:"work1",items:[
-          {n:"item1",r:"r"},
-          {n:"item2",r:"r"},
+    initprojects:[
+      {n:"玩转zigbuild",r:"r1",works:[
+        {n:"renato的c构建翻译",items:[
+          {n:"[]是数组，[*]是什么？",r:"r"},
+          {n:"include c不需要任何声明，怎么做到的？",r:"r"},
+        ]},
+        {n:"cmake",items:[
+          {n:"cmake和make的关系？",r:"r"},
+          {n:"cmake为了解决什么问题而被创建？",r:"r"},
+        ]},
+        {n:"make",items:[
+          {n:"m4是什么？",r:"r"},
+          {n:"make的源代码如何编译？",r:"r"},
         ]}
       ]},
       {n:"project2",r:"r2",works:[{n:"work2",items:[{n:"item1",r:"r211"}]}]},
--- pages/index/index.js
+++ pages/index/index.js
@@ -6,21 +6,31 @@ Page({
   },
   data: {
     ans:[],
+    inputValue:"project descri",
   },
+  inputChange: function (e) {
+    // console.log(e)
+    this.setData({ inputValue: e.detail.value })
+   },
   t: function (e) {
    var id = e.currentTarget.dataset.id
    getApp().globalData.currentProject = getApp().globalData.projects[id]
    wx.navigateTo({url: '../detail/detail',})
   },
   addproject: function (e) {
-     getApp().globalData.projects.push({n:'new 1',r:"rdd1234561"});
+    getApp().addproject({n:this.data.inputValue,r:"rdd1234561"})
+    
      this.refresh();
     },
-    remove: function (e) {
-      var id = e.currentTarget.dataset.id
-      getApp().globalData.projects.splice(id,1)
+    reset: function (e) {
+      getApp().reset();
       this.refresh();
-     },
+      },
+  remove: function (e) {
+    var id = e.currentTarget.dataset.id
+    getApp().globalData.projects.splice(id,1)
+    this.refresh();
+    },
   refresh:function(s){
     this.setData({ ans: getApp().globalData.projects })
   }
